26d8893503bdac8b16ab9413a3a9d4edf4df0726
sonng <sonng@HUYSOWNS>
2025-03-19 09:14:04 +0700

    Add project files.

---
+++ TH_03/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using TH_03.Models;
+
+namespace TH_03.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+}
